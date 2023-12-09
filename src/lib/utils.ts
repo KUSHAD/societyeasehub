833ab9e3cfeef9bbe5aa34a79bcbb57261fbe6c2
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2023-12-09 18:32:16 +0530

    checking if the stripe webhook works

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { getBaseUrl } from "~/trpc/shared";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function absoluteUrl(path: string) {
-  if (typeof window !== "undefined") return path;
-  if (process.env.VERCEL_UR) return `https://${process.env.VERCEL_UR}${path}`;
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+  return `${getBaseUrl()}${path}`;
 }
