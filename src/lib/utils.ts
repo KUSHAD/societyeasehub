3e67cdac74295a592adc0a8747f7b3a3ae42bb11
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2023-12-09 18:40:25 +0530

    hoping stripe paymentworks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { getBaseUrl } from "~/trpc/shared";
+import { env } from "~/env";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function absoluteUrl(path: string) {
-  return `${getBaseUrl()}${path}`;
+  const url =
+    env.NODE_ENV === "production"
+      ? "https://flat-management-system.vercel.app"
+      : "http://localhost:3000";
+  return `${url}${path}`;
 }
