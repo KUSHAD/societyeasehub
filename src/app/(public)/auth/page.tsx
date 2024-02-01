239a038b6eef6384b58beeae3f66830ba6bdc6a6
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2024-02-01 17:35:11 +0530

    improved metadata and created pircing page

--- src/app/(public)/auth/page.tsx
+++ src/app/(public)/auth/page.tsx
@@ -1,6 +1,22 @@
+import { type Metadata } from "next";
 import ClientOnly from "~/components/ClientOnly";
 import AuthButtons from "~/components/auth/AuthButtons";
 import AuthError from "~/components/auth/AuthError";
+
+export const metadata: Metadata = {
+  title: "Authentication | SocietyEaseHub",
+  description:
+    "Secure and seamless authentication for your society management needs. Explore our advanced authentication features, ensuring the protection of your community's sensitive data. Trust SocietyEase Hub for reliable access control.",
+  keywords: [
+    "Authentication system",
+    "Secure login",
+    "Access control",
+    "Community authentication",
+    "User verification",
+    "Society security",
+    "Trusted authentication solution",
+  ],
+};
 
 export default function Page() {
   return (
--- src/app/(public)/pricing/page.tsx
+++ src/app/(public)/pricing/page.tsx
@@ -1,3 +1,22 @@
+import { type Metadata } from "next";
+import { Pricing } from "~/components/pricing/Pricing";
+
+export const metadata: Metadata = {
+  title: "Pricing | SocietyEaseHub",
+  description:
+    "Explore transparent and flexible pricing plans for SocietyEase Hub, your all-in-one SaaS solution for society management. Choose the perfect plan to fit your community's needs and budget.",
+  keywords: [
+    "SaaS pricing",
+    "Society management plans",
+    "Flexible pricing",
+    "Community software costs",
+    "Transparent pricing",
+    "Affordable community solutions",
+    "Pricing tiers",
+    "Budget-friendly society management",
+  ],
+};
+
 export default function Page() {
-  return <div>Pricing</div>;
+  return <Pricing />;
 }
