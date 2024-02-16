b981a14931e7c1885c7a5d2558129e67afe97c39
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2024-02-16 19:05:47 +0530

    fix ts errors

--- src/server/api/routers/role.ts
+++ src/server/api/routers/role.ts
@@ -62,6 +62,7 @@ export const rolesRouter = createTRPCRouter({
           createInvite: true,
           assignRole: true,
           kickUser: true,
+          createChannel: true,
           _count: {
             select: {
               members: true,
