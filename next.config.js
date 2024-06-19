da20296c3fcc3a131463eeb5b6039e328166ef6c
AMITAVA CHAKRABORTY <Amitava.Chakraborty@kyndryl.com>
2024-06-19 12:45:53 +0530

    trying to understand the exact digest prop err

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-// Import environment variables if not skipping validation
-if (!process.env.SKIP_ENV_VALIDATION) {
-  await import("./src/env.js");
-}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
@@ -13,9 +14,9 @@ const nextConfig = {
   },
   experimental: {
     ppr: true,
+    typedRoutes: true,
     useWasmBinary: true,
     cssChunking: "strict",
-    typedRoutes: true,
   },
   images: {
     remotePatterns: [
@@ -37,43 +38,4 @@ const nextConfig = {
   },
 };
 
-// Import the withSentryConfig function using ES module syntax
-import { withSentryConfig } from "@sentry/nextjs";
-
-const sentryConfig = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "kushad",
-  project: "societyeasehub",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-};
-
-// Export the configuration using ES module syntax
-export default withSentryConfig(nextConfig, sentryConfig);
+export default nextConfig;
